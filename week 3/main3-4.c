0ced488f8e98f20b9a86fdf6328d32a0398666a7
hyeonjeong0407-lang <hyeonjeong0407@gmail.com>
2025-08-27 00:39:34 +0900

    week 3-4

---
+++ week 3/main3-4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+
+
+int main(void) {
+    float a, b;
+    float result;
+
+    printf("enter two integers: ");
+    scanf("%f,%f", &a, &b);
+
+    result = a / b;
+
+    printf("%f / %f = %f\n", a, b, result);
+
+    return 0;
+}
