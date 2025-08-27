411f8a8da3c700dfcacc44f48caafcc79e4830a6
hyeonjeong0407-lang <hyeonjeong0407@gmail.com>
2025-08-27 22:45:22 +0900

    week4-4

---
+++ week4/main4-4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+    int year;
+    int result;
+
+    printf("Input the year: ");
+    scanf("%i", &year);
+
+    result = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+
+    printf("Is the year %i the leap year? %i\n", year, result);
+
+    return 0;
+}
